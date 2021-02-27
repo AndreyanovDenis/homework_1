f5630a12d8aeda8022580c565471b8aedf86da2d
Denis Andreyanov <andreyanov.denis92@gmail.com>
2021-02-27 18:55:57 +0300

    Added allure

--- src/test/java/config/BaseTest.java
+++ src/test/java/config/BaseTest.java
@@ -1,18 +1,43 @@
 package config;
 
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Configuration.headless;
 import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentHelper.*;
 
 public class BaseTest {
 
     @BeforeAll
     static void setup() {
-        headless = true;
-        open("https://demoqa.com/automation-practice-form");
+        addListener("AllureSelenide", new AllureSelenide());
+        Configuration.browser = System.getProperty("browser", "chrome");
+        if (System.getProperty("remote_driver") != null) {
+            // config for Java + Selenide
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = System.getProperty("remote_driver");
+            open("https://demoqa.com/automation-practice-form");
+
+        }
+
     }
 
-
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
+    }
 }
 
